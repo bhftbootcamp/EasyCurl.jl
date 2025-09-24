8835ae5e9e6274758380d6812e49977651df8ff6
luigikav <90866467+luigikav@users.noreply.github.com>
2025-09-24 20:19:16 +0200

    Update src/EasyCurl.jl
    Co-authored-by: Plaunov Stephan <styopa.plaunov@gmail.com>

--- src/EasyCurl.jl
+++ src/EasyCurl.jl
@@ -290,7 +290,7 @@ Represents an error from a libcurl multi interface call.
 ## Fields
 - `code::Int`: The libcurl multi error code.
 - `message::String`: The corresponding error message from libcurl.
-- `diagnostics::Union{CurlDiagnostics,Nothing}`: diagnostic message, that will contain virtually all context info if available
+- `diagnostics::CurlDiagnostics`: diagnostic struct, that will contain virtually all context info if available
 
 ## Examples
 
